36912399b1d9ae3dab6a0f79007b1cb113667b02
Ferhat <40534918+aydogan4288@users.noreply.github.com>
2018-11-10 15:13:33 -0500

    Add files via upload

---
+++ djangotodoapp/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length =50, verbose_name = 'Header')
+    completed = models.BooleanField(verbose_name = "Status")
+
+    def __str__(self):
+        return self.title
+    
+
